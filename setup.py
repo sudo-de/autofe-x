4f4539a549f29704dc262fa36163273c5ffe1f9c
Sudo <sudo.sde@gmail.com>
2025-12-09 01:13:15 +0530

    Bump version to 0.1.1 for PyPI release

--- setup.py
+++ setup.py
@@ -49,7 +49,7 @@ except FileNotFoundError:
 
 setup(
     name="autofex",
-    version="0.1.2",
+    version="0.1.1",
     author="AutoFE-X",
     author_email="sudo.dev26@gmail.com",
     description="Automated Feature Engineering + Data Profiling + Leakage Detection",
